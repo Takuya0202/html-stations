b4d98ce8455f1c57a90a3d48691d06175106867d
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-05-31 23:16:38 +0900

    html,css,js入門クリア

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,3 +1,5 @@
+const { console } = require("fp-ts");
+
 async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
@@ -6,11 +8,20 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+async function test(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    },3000);
+  }).then(() => {
+    const arr = data.map((elem) => ({
+      full_name : `${elem.family_name} ${elem.first_name}`,
+    }));
+    return arr;
+  });
 }
 
